1fb8ee6d42f67685b80798fc1c3494a1c4d89059
Shrawan2000 <122910964+Shrawan2000@users.noreply.github.com>
2023-06-22 01:11:03 +0530

    New committed

---
+++ project/script.js
@@ -0,0 +1,9 @@
+// ======================== typing animation ================================
+
+var typed= new Typed(".typing",{
+    strings:["","web Designer","Web Developer","Graphics Designer","java developer","Python Developer"]
+    , 
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
